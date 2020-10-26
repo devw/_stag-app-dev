b62f6d20f876874ce087c4439cace4a0e2bdfedb
Antonio Pierro <antonio.pierro@gmail.com>
2020-10-26 12:38:59 +0100

    refactoring script-tag api

--- pages/api/script-tag.ts
+++ pages/api/script-tag.ts
@@ -1,50 +1,15 @@
-const dotenv = require("dotenv");
-dotenv.config();
-const Shopify = require("shopify-api-node");
-const { SHOPIFY_API_SECRET, SHOPIFY_API_KEY, SHOP, SCRIPT_TAG } = process.env;
-const { scriptTag } = new Shopify({
-    shopName: SHOP,
-    apiKey: SHOPIFY_API_KEY,
-    password: SHOPIFY_API_SECRET,
-});
+import {NextApiRequest, NextApiResponse} from 'next';
 
-const API = {
-    list: async () => {
-        const scripts = await scriptTag.list({ src: SCRIPT_TAG });
-        console.log("sqasdasd");
-        return scripts;
-        return `ScriptTags' list: ${JSON.stringify(scripts)}.`;
-    },
-    create: async () => {
-        const param = { event: "onload", src: SCRIPT_TAG };
-        const response = await scriptTag.create(param);
-        return `Created the following script: ${JSON.stringify(response)}`;
-    },
-    clean: async () => {
-        const scripts = await scriptTag.list({ src: SCRIPT_TAG });
-        const ids = scripts.map((e) => e.id);
-        ids.map((id) => scriptTag.delete(id));
-        return `Removed the following scripts: ${ids}.`;
-    },
-};
-
-// (async () => {
-//     const questions = [
-//         {
-//             type: "select",
-//             name: "scriptTag",
-//             message: `Choose what to do with the following scriptTag "${SCRIPT_TAG}" and shop "${SHOP}"?`,
-//             choices: Reflect.ownKeys(API).map((e) => ({
-//                 title: `${e} scriptTag`,
-//                 value: e,
-//             })),
-//         },
-//     ];
-//     const onSubmit = async (_, answer) => console.log(await API[answer]());
-//     const response = await prompts(questions, { onSubmit });
-// })();
-
-export default (req, res) => {
-    console.log("SHOPIFY_API_SECRET", SHOPIFY_API_SECRET);
-    res.status(200).json({ asda: 2 });
-};
+export default async  (req: NextApiRequest, res: NextApiResponse ) => {
+    const {accessToken, SHOP, SCRIPT_TAG} = process.env; 
+    const Shopify = require("shopify-api-node");
+    const { scriptTag } = new Shopify({
+        shopName: SHOP,
+        accessToken: accessToken,
+    });
+    try {
+        const data = await scriptTag.list({ src: "asdasds" });
+    } catch (e) {
+        return res.status(500).json({data: [{error :"Internal Server Error "}]});
+    }
+}
--- pages/script-tag.tsx
+++ pages/script-tag.tsx
@@ -1,21 +1,15 @@
+import React from "react";
 import { JS2TBL } from "./utils";
-import fixture from "./fixtures";
 
-const ScriptTag = ({ data = fixture.scriptTag }) => {
-    return <JS2TBL props={{ json: data, title: "script Tag" }} />
+export default class ScriptTag extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    async componentDidMount () {
+        const result = await fetch("/api/script-tag");
+        this.setState(await result.json());
+    }
+
+    render = () => <JS2TBL json={this.state?.data} title="Script Tag" />
 }
-    
-export async function getServerSideProps() {
-    const Shopify = require("shopify-api-node");
-    const { scriptTag } = new Shopify({
-        shopName: process.env.SHOP,
-        accessToken: "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", // you should get it from aws:dynamo
-    });
-    const SCRIPT_TAG = "https://devw.github.io/script-tag/bundle.js"; // you should get it from aws:dynamo
-    const data = await scriptTag.list({ src: SCRIPT_TAG });
-    console.log("ScriptTag", process.env.SHOP, data);
-
-    return { props: { data } };
-}
-
-export default ScriptTag;
