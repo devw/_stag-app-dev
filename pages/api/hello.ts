0f1578c0de208505d8b48a611e7c6a7c03c6b475
Antonio Pierro <antonio.pierro@gmail.com>
2020-10-26 14:32:46 +0100

    refactoring

--- pages/api/hello.ts
+++
@@ -1,7 +0,0 @@
-import {NextApiRequest, NextApiResponse} from 'next';
-
-export default (req: NextApiRequest, res:NextApiResponse ) => {
-    console.log("server-side", process.env.SHOP);
-    res.setHeader("X-Customer-Header", "wqeq");
-    res.status(200).json({ text: 'hello' });
-};
